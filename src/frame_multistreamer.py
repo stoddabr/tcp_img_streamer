559bfaea6e1dd963367a018086d6a9942f28f162
stoddabr <stoddabr@oregonstate.edu>
2022-02-16 21:46:10 +0000

    single thread working

--- src/frame_multistreamer.py
+++ src/frame_multistreamer.py
@@ -12,23 +12,31 @@ from sensor_msgs.msg import Image
 import logging 
 import sys
 
-# setup multi-thread logging
+# setup multi-threading
+threads = []
+RUN_BACKGROUND_THREADS = True  # kill flag
+def initThread(t):
+    t.daemon = True  # enable threads to be killed via ctrl+c
+    t.start()
+    threads.append(t)
+
+def killAllThreads():
+    # set flag for threads to die piecefully
+    RUN_BACKGROUND_THREADS = False  # kill threads
+
+    # raising exception isn't working (and is violent)
+    # for t in threads:
+    #     t.raise_exception()
 
 
-# setup socket 
+# setup global socket variables 
 TCP_IP = '172.17.0.2'  # faster than dedicated ip address https://docs.python.org/3/howto/sockets.html#ipc
 TCP_PORT = 5010
 host_name = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
 MESSAGE = ''  # message to be sent to socket clients
 NEW_IMAGE_RECIEVED = False  # logic to only send if image is new
-
-
-# create socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(5)  # max 5 connections
-    # TODO optimize muxing over multiple client sockets
+RUN_BACKGROUND_THREADS = True  # 
 
 
 # setup cv2 bridge
@@ -72,10 +80,62 @@ def init_ros(img_topic='/color/image_raw'):   # TODO pass topic from launch file
 threading.Thread(target=init_ros).start()
 
 
+
+
+# setup and run multi-threaded socket
+# create socket globals
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(5)  # max 5 connections
+    # TODO optimize muxing over multiple client sockets
+
+def on_new_client(clientsocket, addr):
+    global MESSAGE
+    global RUN_BACKGROUND_THREADS
+
+    print('init client')
+    while RUN_BACKGROUND_THREADS:
+        time.sleep(0.05)  # avoid latency caused by tcp traffic
+
+        if clientsocket and len(MESSAGE) > 100 and NEW_IMAGE_RECIEVED:
+            # send image and prevent sending duplicates
+            print('Sending:', MESSAGE[:20])
+            clientsocket.sendall(MESSAGE)
+            NEW_IMAGE_RECIEVED = False 
+
+
+def server_listen():
+    # start listening
+    # accept connections
+    try:
+        clientsocket, addr = s.accept()
+    except Exception as e:
+        # s.accept() timed out so try again
+        # part of a hack to keep s.accept() from blocking thread
+        if str(e) == 'timed out': 
+            return  # try again 
+    if clientsocket:
+        tc = threading.Thread(target=on_new_client, args=(clientsocket, addr,))
+        initThread(tc)    
+
 # start listening
 print('HOST IP: '+ host_ip)
 print('HOST Port: '+ str(TCP_PORT))
 print('Address: '+ host_ip+':'+str(TCP_PORT)+'/')
+
+# main script
+try:
+    while True:
+        server_listen()
+except KeyboardInterrupt:
+    print('exception triggered')
+    killAllThreads()
+except Exception as e:
+    print('error in main loop:', e)
+s.close()
+cv2.destroyAllWindows() 
+os._exit('error')
+
 while True:
     # accept connections
     clientsocket, addr = s.accept()  # blocking
